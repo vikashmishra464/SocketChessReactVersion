34dff4d78c73fc62e0b7df4da7646d6240494f28
vikashmishra464 <vikashmishra0200@gmail.com>
2025-04-05 00:47:50 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<EntryPage />} />
+        <Route index path="/" element={<EntryPage />} />
         <Route path="/play" element={<ChessArea/>} />
         
       </Routes>
